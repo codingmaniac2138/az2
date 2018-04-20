0e5c0e29dfe43d4679955212ebf091ca605e195e
suhasjadhav23 <suhasjadhav23@gmail.com>
2018-04-20 12:50:12 +0530

    added filter on checkbox and search box

--- src/app/components/codingreview_module/crtable/crtable.component.ts
+++ src/app/components/codingreview_module/crtable/crtable.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { exists } from 'fs';
 
 @Component({
   selector: 'app-crtable',
@@ -45,38 +46,112 @@ this.data_set =[{ "First Name": "John","Last Name":"Doe", "Discharge Date": "11/
       console.log(this.character)
       
       // document.getElementsByTagName('td').inn;
-   
+
   }
 
   // Handling the on click event on the row
   onRowClicked(row) {
     console.log("the row is: "+ JSON.stringify(row));
     
+ 
   }
+       // function to make search on the basis of checkboxes
+read_checks(){
+
+var c=document.getElementById('new');
+  var d=document.getElementById('progress');
+  if (c.checked) {
+    // alert("new");
+     return this.checkbox_filter("New")
+    ;
+  } 
+  else {
+    // alert("ooooooooooooo");
+    if(d.checked){ 
+      // alert("progress");  
+      return this.checkbox_filter("In Progress");
+    }
+    else {
+      // alert("unchecked");
+      
+        return this.checkbox_filter("");
+  }
+}
+}
+  // function which searches on the whole table
   myFunction() {
+    // -----------------------------------------------------------------------------
+ 
+    // -------------------------------------------------------------------------------------
     var input, filter, table, tr, td,td1,td2,td3, i;
     input = document.getElementById("myInput");
+
+
     filter = input.value.toUpperCase();
+    if( filter != "" ){
+
+    table = document.getElementById("tb");
+    tr = table.getElementsByTagName("tr");
+    // alert(tr.length);
+
+    for (i = 0; i < tr.length; i++) {
+      if (tr[i].style.display != "none"){
+      td = tr[i].getElementsByTagName("td")[1];
+      td1 = tr[i].getElementsByTagName("td")[2];
+      td2 = tr[i].getElementsByTagName("td")[3];
+      td3 = tr[i].getElementsByTagName("td")[0];
+
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    // alert("inside innerhtml");
+
+          tr[i].style.display = "";
+        }           
+        else if(td1.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          // alert("inside innerhtml");
+
+          tr[i].style.display = "";
+         }
+         else if(td2.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          // alert("inside innerhtml");
+          tr[i].style.display = "";
+         }
+        //  else if(td3.innerHTML.toUpperCase().indexOf(filter) > -1) {
+
+        //   tr[i].style.display = "";
+        //  }
+          else {
+            // alert("keeeeeeeeeeeeeeeppppppppppp newwwwwwwwwwwww")
+            // this.read_checks()
+          tr[i].style.display = "none";
+          
+        }
+
+      } 
+    }
+    }
+  }
+  else {
+    // alert("search is empty");
+    this.read_checks()
+  }
+  }
+  // function to filter as per check boxes
+  checkbox_filter(check_value) {
+    // alert(check_value);
+    var input, filter, table, tr, td,td1,td2,td3, i;
+    input = document.getElementById("myInput");
+    filter = check_value.toUpperCase();
     table = document.getElementById("tb");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[0];
-      td1 = tr[i].getElementsByTagName("td")[1];
-      td2 = tr[i].getElementsByTagName("td")[2];
-      td3 = tr[i].getElementsByTagName("td")[3];
-
+     
+      
       if (td) {
         if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
           tr[i].style.display = "";
-        }           
-        else if(td1.innerHTML.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-         }
-         else if(td2.innerHTML.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-         }
-         else if(td3.innerHTML.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
+
          }
           else {
           tr[i].style.display = "none";
@@ -85,61 +160,4 @@ this.data_set =[{ "First Name": "John","Last Name":"Doe", "Discharge Date": "11/
       
     }
   }
-  
 }
-
-
-
-
-// // list of rows which should be displayed
-// public rows: Array<any> = [];
-  
-// // the columns which contain the config part like title of the columns and sort action true or false.
-// public columns: Array<any> = [
-//   {title: "FirstName", name: "first_name"},
-//   {title: "LastName", name: "last_name", sort: false},
-//   {title: "EmployeeID", name: "employee_id", sort: false},
-//   {title: "Role", name: "role", sort: false}
-// ];
-
-// // the default page after the component page loading
-// public page: number = 1;
-
-// // number of items available on the page
-// public itemsPerPage: number = 10;
-
-// // number in the pagination list after which ... will appear
-// public maxSize: number = 4;
-
-// // the number of pages that are there in the view right now only 1 for testing
-// public numPages: number = 1;
-
-// // the total number of items after filtering (not required by current version may be in the next version).
-// public length: number = 0;
-
-// // the config object that defines the configuration about the table sorting, filtering and pagination
-// public config = {
-//   paging: true,
-//   sorting: { columns: this.columns },
-// };
-
-// // the data source that we add show on the table after querying the backend REST service.Right now testing with dummy data
-// public dataSource: Array<any> = DummyData;
-
-// // 
-// constructor() {
-//   this.length = this.dataSource.length;
-// }
-
-// ngOnInit() {
-// }
-
-// public onChangeTable(config:any) {
-//   // const page: any = {page: this.page, itemsPerPage: this.itemsPerPage};
-  
-//   // if (config.sorting) {
-//   //   Object.assign(this.config.sorting, config.sorting);
-//   // }
-//   console.log("Inside the onTableChange function");
-
-// }
